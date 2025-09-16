6736c3af635cea17c6fcb7c1292d75907f634105
terumichi1209 <terumichi1209@gmail.com>
2025-09-16 17:35:20 +0900

    パス修正

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Terumichi Ishibashi Portfolio</title>
         <meta name="description" content="The web development portfolio of Terumichi Ishibashi." />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Geist:wght@400;700&family=Geist+Mono&display=swap" />
       </Head>
       <div className="antialiased">
         <Component {...pageProps} />
